864f1f8a5bf3e199f40adf5c60a39996f03671f7
Aditya Gupta <adityagupta2087@gmail.com>
2025-12-24 00:28:08 +0000

    created window

--- main.c
+++ main.c
@@ -3,4 +3,15 @@
 int main() { 
 	InitWindow(600, 400, "ray tracer");
 
+	SetTargetFPS(60); 
+
+	while(!WindowShouldClose()) { 
+		BeginDrawing();
+	       		ClearBackground(BLACK); 
+			DrawFPS(20, 20); 	
+		EndDrawing(); 
+	}
+	
+	CloseWindow(); 
+	return 0; 
 }
